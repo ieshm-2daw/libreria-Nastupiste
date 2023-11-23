3d41f9eee9a0aff900f281c5ec7c8c72ebfe223b
Nastupiste <mjroscos@gmail.com>
2023-11-23 14:02:31 +0100

    modelo terminado (sin PK,FK)

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
-"""
 
-1. Usuario: Esta clase debe extender AbstractUser de Django e incluir los campos adicionales como dni, dirección,
- y teléfono.
+"""
+1. Usuario: Esta clase debe extender AbstractUser de Django e incluir los campos adicionales como dni, dirección y teléfono.
 """
 
 
@@ -16,18 +17,6 @@ class Usuario(AbstractUser, models.Model):
 
     def __str__(self):
         return self.dni
-
-
-"""
-2. Libro: Un modelo que represente los libros de la biblioteca. Debe tener campos como título, autor(es),
-editorial, fecha de publicación, género, ISBN, resumen, disponibilidad (posibles valores: disponible, prestado, 
-en proceso de préstamo), portada, etc.
-"""
-
-
-class Libro(models.Model):
-    titulo = models.CharField(max_length=150).primary_key
-    autores = models.ForeignKey()
 
 
 """
@@ -47,6 +36,8 @@ class Autor(models.Model):
 """
 4. Editorial: con los campos nombre, dirección y sitio web.
 """
+
+
 class Editorial(models.Model):
     nombre = models.CharField(max_length=150)
     direccion = models.CharField(max_length=250)
@@ -55,15 +46,44 @@ class Editorial(models.Model):
     def __str__(self):
         return self.nombre
 
+
+"""
+2. Libro: Un modelo que represente los libros de la biblioteca. Debe tener campos como título, autor(es),
+editorial, fecha de publicación, género, ISBN, resumen, disponibilidad (posibles valores: disponible, prestado, 
+en proceso de préstamo), portada, etc.
+"""
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=150).primary_key
+    autores = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+    fechaPublicacion = models.DateField()
+    genero = models.CharField(max_length=100)
+    isbn = models.IntegerField(validators=[MinValueValidator(
+        1000000000000), MaxValueValidator(9999999999999)])
+    resumen = models.TextField()
+    disponibilidad = models.CharField(
+        choices=["Disponible", "Prestado", "En Proceso de Préstamo"])
+    portada = models.ImageField()
+
+    def __str__(self):
+        return self.titulo
+
+
 """
 5. Préstamo: Un modelo para registrar los préstamos de libros a los usuarios. Debe contener campos para el 
 libro prestado, la fecha de préstamo, la fecha de devolución, el usuario que lo prestó y el estado del préstamo 
 (prestado, devuelto).
 """
 
+
 class Prestamo(models.Model):
-    libroPrestado=models.ForeignKey(Libro,on_delete=)
-    fechaPrestamo=models.DateField()
-    fechaDevolucion=models.DateField()
-    usuario=models.ForeignKey(Usuario,on_delete=)
-    
+    libroPrestado = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    fechaPrestamo = models.DateField()
+    fechaDevolucion = models.DateField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    estado = models.CharField(choices=['Prestado', 'Devuelto'])
+
+    def __str__(self):
+        return self.estado
--- nastu_site/settings.py
+++ nastu_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^hzuspm!lz8!=wg!ta1)uoer6ko6c+s$%i%ni1!@t88b*4of9m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'biblioteca.apps.BibliotecaConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'Europe/Spain'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
